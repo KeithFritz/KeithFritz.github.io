a165c945c6e649621159d94c5537e53a732f51e5
Keith Fritz <keith.fritz@me.com.com>
2024-09-13 15:22:31 -0500

    Added carousel functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -247,4 +247,40 @@
 		$banner
 			._parallax();
 
+	// Accessibility Improvement: Adding keyboard navigation for better user experience
+		$('#nav a').on('focus blur', function() {
+			$(this).parent().toggleClass('focus');
+		});
+
+	// Carousel/Slideshow.
+	let slideIndex = 1;
+	showSlides(slideIndex);
+
+	function plusSlides(n) {
+		showSlides(slideIndex += n);
+	}
+
+	function currentSlide(n) {
+		showSlides(slideIndex = n);
+	}
+
+	function showSlides(n) {
+		let slides = document.getElementsByClassName("slide");
+		let dots = document.getElementsByClassName("dot");
+
+		if (n > slides.length) { slideIndex = 1 }
+		if (n < 1) { slideIndex = slides.length }
+
+		for (let i = 0; i < slides.length; i++) {
+			slides[i].style.display = "none";
+		}
+
+		for (let i = 0; i < dots.length; i++) {
+			dots[i].className = dots[i].className.replace(" active-dot", "");
+		}
+
+		slides[slideIndex - 1].style.display = "block";
+		dots[slideIndex - 1].className += " active-dot";
+	}
+
 })(jQuery);
